130d38b40d8cf73120eb798e0adaa1bfc8b83ac5
Subhadeep <subhadeep0326@gmail.com>
2021-11-09 00:18:57 +0530

    Subhadeep added parameterised constructor by reflection

--- MoodAnalyser01Test_Core/UnitTest1.cs
+++ MoodAnalyser01Test_Core/UnitTest1.cs
@@ -126,7 +126,7 @@ namespace MoodAnalyser01Test_Core
         }
 
         [TestMethod]
-        //Test Case 4.3 Given wrong constructor name should return simproper message in exception  
+        //Test Case 4.3 Given wrong constructor name should return improper message in exception  
 
         public void GivenMoodAnalyseWrongConstructor_Should_return_MoodAnalyseObjectException_Message()
         {
@@ -144,5 +144,52 @@ namespace MoodAnalyser01Test_Core
             }
         }
 
+
+        [TestMethod]
+        //Test Case 5.1 given mood Analyse class name should return mood analyser object with parameterised constructor
+
+        public void Given_ParameterisedConstructor_MoodAnalyseClassName_Should_return_MoodAnalyseObject()
+        {
+            object expected = new MoodAnalyser("HAPPY");
+            object obj = MoodAnalyserFactory.CreateMoodAnalyserParameterisedConstructor("MoodAnalyser01_Core.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(obj);
+        }
+
+
+        [TestMethod]
+        //Test Case 5.2 Given mood Analyse wrong class name should return exception stating no such class name exist 
+
+        public void Given_ParameterisedConstructor_WrongClassName_Should_return_MoodAnalyseObjectException_Message()
+        {
+            try
+            {
+                object expected = new MoodAnalyser("HAPPY");
+                object obj = MoodAnalyserFactory.CreateMoodAnalyserParameterisedConstructor("MoodAnalyser01_Core.MoodAnalyserWrong", "MoodAnalyser");
+                expected.Equals(obj);
+            }
+
+            catch (CustomMoodAnException e)
+            {
+                Assert.AreEqual("Class not found", e.Message);
+            }
+        }
+
+
+        //Test Case 5.3 Given mood Analyse wrong constructor name should return exception stating no such consrtructor exist 
+        [TestMethod]
+        public void Given_ParameterisedConstructor_WrongConstrcutorName_Should_return_MoodAnalyseObjectException_Message()
+        {
+            try
+            {
+                object expected = new MoodAnalyser("HAPPY");
+                object obj = MoodAnalyserFactory.CreateMoodAnalyserParameterisedConstructor("MoodAnalyser01_Core.MoodAnalyser", "MoodAnalyserWrong");
+                expected.Equals(obj);
+            }
+
+            catch (CustomMoodAnException e)
+            {
+                Assert.AreEqual("Constructor not found", e.Message);
+            }
+        }
     }
 }
